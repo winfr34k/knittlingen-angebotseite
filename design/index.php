91198c0cd7943db8bf10bddb0079d6123f981009
Thomas Keppler <winfr34k@gmail.com>
2014-10-19 19:02:00 +0200

    - Aufräumarbeiten in der CSS / den Dateien 	=> Unnötige Inhalte entfernt 	=> HTML Dateien semantisch korrekt aktualisiert 	=> CSS Dateien gekürzt/geändert für besseres look&feel

--- design/index.php
+++ design/index.php
@@ -3,26 +3,28 @@
 <head>
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
+	
 	<link rel="stylesheet" href="css/bootstrap.min.css">
 	<link rel="stylesheet" href="css/stylesheet.css">
 	<link rel="stylesheet" href="css/bootstrap-theme.min.css">
-	  <link rel="stylesheet" href="//code.jquery.com/ui/1.11.2/themes/smoothness/jquery-ui.css">
-  <script src="//code.jquery.com/jquery-1.10.2.js"></script>
-  <script src="//code.jquery.com/ui/1.11.2/jquery-ui.js"></script>
+	<link rel="stylesheet" href="//code.jquery.com/ui/1.11.2/themes/smoothness/jquery-ui.css">
+  	
+  	<script src="//code.jquery.com/jquery-1.10.2.js"></script>
+  	<script src="//code.jquery.com/ui/1.11.2/jquery-ui.js"></script>
 	<script src="js/bootstrap.min.js"></script>
 	  <script>
 		  $(function() {
 			$( "#accordion" ).accordion();
 		  });
 		</script>
+	
 	<title>Adventsangebote - GVV</title>
 </head>
 <body>
 	<div id="mainarea">
-
-		<div class="header"></div>
+		<div id="header"></div>
 		
-		<div class="content">
+		<div id="content">
 			<h2> <center>Weihnachtsangebote der Gewerbetreibenden in Knittlingen </center></h2>
 			<h4> <center>Arbeitsgruppe GVV Knittlingen 2020</center></h4>
 			<div id="accordion">
@@ -34,7 +36,7 @@
 				<div class="descripton">
 					Beschreibung, Beschreibung, Beschreibung, bablab jsdljdasfkljdsf dsflj dsfkljdsf kjdfskljdsfdfs dksf dsf dsfdfas dfsdfsdfas dfs dfs ds dsklj dfsklj dfs dfs dfsdfs das fds fkjds flkjdsfkljasdjsd fkljda fljsd faj dasflkj dasflkjasdfl kjasdlfj dkjas flkj lkdjasf jklsdjfa lkjsdfl kjasflksdfja lksdfjolijbkji sawelji jsd fkjlsrfj3liw fjklsd fjlsdkjf 
 				</div>
-				<div class="firma">
+				<div class="company">
 					<a href="#">Firma & Co KG</a>
 				</div>
 				<div class="price">
@@ -48,9 +50,9 @@
 					GÃ¼ltig bis 12:12 Uhr am 23.10.1999
 				</div>
 				<div class="descripton">
-					Beschreibung, Beschreibung, Beschreibung, bablab jsdlfjdasfkljdsf dsflj dsfkljdsf kjdfskljdsfdfs döksf dsf dsfdfas dfsdfsdfas dfs dfs ds dsklj dfsklj dfs dfs dfsdfs das fds fkjds flkjdsfkljasdöjsd fkljda följsd faj dasflkj dasflkjasdöfl kjasdölfj dkjas fölkj ölkdjasf jklsdjfa ölkjsdfl kjasfölksdfja lksdfjöolijbölkji sawöelji jsdöl fkjölsrfj3öliw fjklsd fjlsdkjf 
+					Beschreibung, Beschreibung, Beschreibung, bablab jsdlfjdasfkljdsf dsflj dsfkljdsf kjdfskljdsfdfs d?ksf dsf dsfdfas dfsdfsdfas dfs dfs ds dsklj dfsklj dfs dfs dfsdfs das fds fkjds flkjdsfkljasd?jsd fkljda f?ljsd faj dasflkj dasflkjasd?fl kjasd?lfj dkjas f?lkj ?lkdjasf jklsdjfa ?lkjsdfl kjasf?lksdfja lksdfj?olijb?lkji saw?elji jsd?l fkj?lsrfj3?liw fjklsd fjlsdkjf 
 				</div>
-				<div class="firma">
+				<div class="company">
 					<a href="#">Firma & Co KG</a>
 				</div>
 				<div class="price">
@@ -61,7 +63,7 @@
 		</div>
 
 
-		<div class="muted">
+		<div id="muted">
 			Weihnachtsangebote des Gewerbe- und Verkehrsverein Knittlingen | <a href="/impressum.php"> Impressum</a>
 		</div>
 	</div>
--- design/user.php
+++ design/user.php
@@ -3,21 +3,28 @@
 <head>
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
+	
 	<link rel="stylesheet" href="css/bootstrap.min.css">
 	<link rel="stylesheet" href="css/stylesheet.css">
 	<link rel="stylesheet" href="css/bootstrap-theme.min.css">
+	<link rel="stylesheet" href="//code.jquery.com/ui/1.11.2/themes/smoothness/jquery-ui.css">
+  	
+  	<script src="//code.jquery.com/jquery-1.10.2.js"></script>
+  	<script src="//code.jquery.com/ui/1.11.2/jquery-ui.js"></script>
 	<script src="js/bootstrap.min.js"></script>
-	<title>Benutzerverwaltung</title>
+	  <script>
+		  $(function() {
+			$( "#accordion" ).accordion();
+		  });
+		</script>
+	
+	<title>Nutzerverwaltung - GVV</title>
 </head>
 <body>
 	<div id="mainarea">
+		<div id="header"></div>
 
-		<div class="header">
-
-		</div>
-		<div class="navbar"
-		</div>
-		<div class="content">
+		<div id="content">
 			<h3> Gewerbe Art Anlegen</h3>
 			<form>
 				<input type="text" class="form-control" placeholder="Art des Gewerbes">
@@ -56,7 +63,7 @@
 		</div>
 	</div>
    
-		<div class="muted">
+		<div id="muted">
 			Adventsangebote des Gewerbe- und Verkehrsverein Knittlingen | <a href="/impressum.php"> Impressum</a>
 		</div>
 </body>
