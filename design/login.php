4f81488609ecaf8b6b914ecee72bf486965b2461
unknown <Christoph@Christoph-PC-01.walenta.local>
2014-10-19 13:15:14 +0200

    => Login Formular (Test)

---
+++ design/login.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang="de">
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" href="css/bootstrap.min.css">
+	<link rel="stylesheet" href="css/stylesheet.css">
+	<link rel="stylesheet" href="css/bootstrap-theme.min.css">
+	<script src="js/bootstrap.min.js"></script>
+	<title>Login - Advent</title>
+</head>
+<body>
+	<div id="mainarea">
+
+		<div class="header">
+
+		</div>
+		<div class="navbar"
+		</div>
+		<div class="content">
+			<form class="form-horizontal" role="form">
+			  <div class="form-group">
+				<label for="inputEmail3" class="col-sm-2 control-label">Benutzer</label>
+				<div class="col-sm-10">
+				  <input type="email" class="form-control" id="inputEmail3" placeholder="Email">
+				</div>
+			  </div>
+			  <div class="form-group">
+				<label for="inputPassword3" class="col-sm-2 control-label">Passwort</label>
+				<div class="col-sm-10">
+				  <input type="password" class="form-control" id="inputPassword3" placeholder="Password">
+				</div>
+			  </div>
+			  <div class="form-group">
+				<div class="col-sm-offset-2 col-sm-10">
+				  <div class="checkbox">
+				  </div>
+				</div>
+			  </div>
+			  <div class="form-group">
+				<div class="col-sm-offset-2 col-sm-10">
+				  <button type="submit" class="btn btn-default">Sign in</button>
+				</div>
+			  </div>
+			</form>
+		</div>
+	</div>
+   
+		<div class="muted">
+			Adventsangebote des Gewerbe- und Verkehrsverein Knittlingen | <a href="/impressum.php"> Impressum</a>
+		</div>
+</body>
+</html>
