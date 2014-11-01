cc5f47c4a0e80dd78460725230e53acfa889de94
Thomas Keppler <winfr34k@gmail.com>
2014-11-02 00:18:39 +0100

    - Ändern der Config des Mailers

--- app/config/mail.php
+++ app/config/mail.php
@@ -15,7 +15,7 @@ return array(
 	|
 	*/
 
-	'driver' => 'smtp',
+	'driver' => 'log',
 
 	/*
 	|--------------------------------------------------------------------------
@@ -54,7 +54,7 @@ return array(
 	|
 	*/
 
-	'from' => array('address' => null, 'name' => null),
+	'from' => array('address' => 'noreply@knittlingen-2020.de', 'name' => 'Noreply @ Knittlingen-2020'),
 
 	/*
 	|--------------------------------------------------------------------------
@@ -119,6 +119,6 @@ return array(
 	|
 	*/
 
-	'pretend' => false,
+	'pretend' => true,
 
 );
