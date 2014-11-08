dd52600ecddfbcc6201174f602d4cc202e2b85ec
Markus Rauser <PC-Service@Markuss-iMac.fritz.box>
2014-11-08 19:28:54 +0100

    - Hinzufügen des Impressums

--- app/views/frontend/imprint.blade.php
+++ app/views/frontend/imprint.blade.php
@@ -1,5 +1,40 @@
 @extends('master')
 @section('content')
 <h3>Impressum</h3>
-<p>Lorem ipsum dolor sit amet...</p>
+<div id="imprint">
+    <p>
+        <b>Gewerbe- und Verkehrsverein Knittlingen</b><br />
+        Stuttgarter Str. 53 75438 Knittlingen<br />
+        <b>Telefon:</b> 07043 / 31829<br />
+        <b>Telefax:</b> 07043 / 940275<br />
+        <b>E-Mail:</b> <a href="mailto:info@gvv-knittlingen.de">info@gvv-knittlingen.de</a><br />
+        <b>Internet:</b> <a href="http://www.gvv-knittlingen.de">www.gvv-knittlingen.de</a><br />
+        <b>Vorstand:</b> Hagen Jarzambek
+    </p>
+    <p><strong>Hinweis zu allen Links auf dieser Webseite / diesem Server: </strong></p>
+    <p>Mit dem Urteil vom 12. Mai 1998 - 312 O 85/98 - "Haftung für Links" hat das Landgericht (LG) Hamburg entschieden, dass man durch die Erstellung eines Links die Inhalte der gelinkten Seite ggf. mit zu verantworten hat. Dies kann - so das Gericht - nur dadurch verhindert werden, dass man sich ausdrücklich von diesen Inhalten distanziert. Da wir Links zu Geschäftspartnern und sonstigen Webseiten oder Servern besitzen - auf dessen Inhalte und Gestaltung wir keinen Einfluss haben - distanzieren wir uns ausdrücklich von sämtlichen Links auf unserer Webseite / unserem Server. Das gilt für alle vorhandenen / kommenden und dagewesenen Links. Wir weisen deshalb darauf hin, dass wir nicht für Inhalte gelinkter Webseiten verantwortlich gemacht werden können.</p>
+    <p>Copyright © by Gewerbe- und Verkehrsverein Fauststadt Knittlingen e.V.</p>
+    <h4><b>Für Fragen und Anregungen mailen Sie bitte unserem <a href="http://mrauser.net">Webmaster</a>.</b></h4>
+    <hr />
+    <ul style="padding-left: 0;">
+        <li><b>Steuer Nr.:</b> 48050 / 30608</li>
+        <li><b>Gerichtsstand:</b> Amtsgericht Maulbronn</li>
+        <li><b>Gerichtsregister Nr.:</b> GR 71/02</li>
+        <li><b>Vereinsregister Nr.:</b> VR 442</li>
+        <li>
+            <b>Bankverbindung 1:</b> Sparkasse Pforzheim
+            <ul>
+                <li><b>BLZ:</b> 666 500 85</li>
+                <li><b>Kontonummer:</b> 969 435</li>
+            </ul>
+        </li>
+        <li>
+            <b>Bankverbindung 2:</b> Volksbank Pforzheim
+            <ul>
+                <li><b>BLZ:</b> 666 900 00</li>
+                <li><b>Kontonummer:</b> 353 139 7</li>
+            </ul>
+        </li>
+    </ul>
+</div>
 @stop
