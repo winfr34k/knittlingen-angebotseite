780d08a5f7e4fc394848ef97063111b91c8b4bf4
Markus Rauser <PC-Service@Markuss-iMac.fritz.box>
2014-11-08 18:26:09 +0100

    Feature: Hilfe-Tab im ACP

--- app/views/backend/admin.blade.php
+++ app/views/backend/admin.blade.php
@@ -2,12 +2,13 @@
 @section('content')
 <div id="tabs">
   <ul>
-    <li><a href="#myOffers">Angebote verwalten</a></li>
-    <li><a href="#myCompany">Account verwalten</a></li>
+    <li><a href="#myOffers">Angebote</a></li>
+    <li><a href="#myCompany">Account</a></li>
     @if( ! Auth::guest() && Auth::user()->is_admin == 1)
-    <li><a href="#categories">Kategorien verwalten</a></li>
-    <li><a href="#users">Benutzerverwaltung</a>
-    <li><a href="#settings">Systemverwaltung</a></li>
+    <li><a href="#categories">Kategorien</a></li>
+    <li><a href="#users">Benutzer</a>
+    <li><a href="#settings">System</a></li>
+    <li><a href="#help">Hilfe</a></li>
     @endif
   </ul>
 
@@ -17,6 +18,7 @@
   @include('backend.sections.categories')
   @include('backend.sections.users')
   @include('backend.sections.settings')
+  @include('backend.sections.help')
   @endif
 </div>
 @stop
---
+++ app/views/backend/sections/help.blade.php
@@ -0,0 +1,21 @@
+<div id="help">
+	<h3>Hilfe:</h3>
+	<p>Sie sind verzweifelt mit der Bedienung oder etwas funktioniert nicht?<br />Dann kontaktieren Sie uns!</p>
+
+	<table class="table table-responsive">
+	    <tr>
+	        <td><b>MRR PC-Service</b></td>
+	        <td>Geschäftsführer: Markus Rauser</td>
+	    </tr>
+
+        <tr>
+            <td><b>Telefon:</b></td>
+            <td>01805 66 66 315</td>
+        </tr>
+
+        <tr>
+            <td><b>E-Mail:</b></td>
+            <td><a href="mailto:pc-service@mrauser.net">pc-service@mrauser.net</a></td>
+        </tr>
+	</table>
+</div>
