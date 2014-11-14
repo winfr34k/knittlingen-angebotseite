7aaaaa9402c4c6f70105bb54b02956039e452671
Thomas Keppler <winfr34k@gmail.com>
2014-11-14 18:58:32 +0100

    Fix: Jetzt noch mal richtig...

--- app/views/frontend/home.blade.php
+++ app/views/frontend/home.blade.php
@@ -30,12 +30,12 @@
         <tbody>
             @foreach(Offer::where('startDate', '<=', new DateTime('now'))->where('endDate', '>', new DateTime('now'))->get() as $offer)
             <tr>
-                <td>{{ link_to($offer->company->website, $offer->company->name, array('class' => 'btn btn-primary', 'target' => '_blank')) }}</td>
+                <td>{{ link_to($offer->company->website, $offer->company->name, array('target' => '_blank')) }}</td>
                 <td>{{ $offer->name }}</td>
                 <td>{{ date_format(date_create_from_format('Y-m-d H:i:s', $offer->endDate), 'd.m.Y H:i') }} Uhr</td>
                 <td>{{ Str::limit(strip_tags($offer->description), 60) }}</td>
                 <td>{{ number_format($offer->amount, 2, ',', '.') }}€ inkl. MwSt.</td>
-                <td>{{ link_to_route('offers.show', 'Zum Angebot &raquo;', $offer->id) }}</td>
+                <td>{{ link_to_route('offers.show', 'Zum Angebot &raquo;', $offer->id, array('class' => 'btn btn-primary')) }}</td>
             </tr>
             @endforeach
         </tbody>
