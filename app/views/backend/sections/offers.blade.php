a5a705e1e827b695aa3d7687a35b000d2d42f3de
Thomas Keppler <winfr34k@gmail.com>
2014-11-27 18:15:03 +0100

    Test: &nbsp; + Tags strippen?

--- app/views/backend/sections/offers.blade.php
+++ app/views/backend/sections/offers.blade.php
@@ -52,7 +52,7 @@
             <td class="offername">{{ $offer->name }}</td>
             <td class="pricetable">{{ number_format($offer->amount, 2, ',', '.') }}€</td>
             <td class="lapsetable">{{ date_format(date_create_from_format('Y-m-d H:i:s', $offer->endDate), 'd.m.Y H:i') }}</td>
-            <td>{{ Str::limit(strip_tags($offer->description)) }}</td>
+            <td>{{ Str::limit(filter_var($offer->description, FILTER_SANITIZE_STRING)) }}</td>
             <td>{{ $offer->category->name }}</td>
             <td>{{ $offer->company->name  }}</td>
             <td class="edit">
