9a5f0722341eccf8cd22738cd7ade96376603f68
Thomas Keppler <winfr34k@gmail.com>
2014-11-27 18:21:41 +0100

    Fix: Jetzt werden die Tags richtig gestripped. Inklusive &nbsp;.

--- app/views/frontend/home.blade.php
+++ app/views/frontend/home.blade.php
@@ -33,7 +33,7 @@
                 <td>{{ link_to($offer->company->website, $offer->company->name, array('target' => '_blank')) }}</td>
                 <td>{{ $offer->name }}</td>
                 <td>{{ date_format(date_create_from_format('Y-m-d H:i:s', $offer->endDate), 'd.m.Y H:i') }} Uhr</td>
-                <td>{{ Str::limit(strip_tags(htmlspecialchars_decode($offer->description)), 60) }}</td>
+                <td>{{ Str::limit(str_replace('&nbsp;', '', filter_var($offer->description, FILTER_SANITIZE_STRING)), 60) }}</td>
                 <td>{{ number_format($offer->amount, 2, ',', '.') }}€ inkl. MwSt.</td>
                 <td>{{ link_to_route('offers.show', 'Zum Angebot &raquo;', $offer->id, array('class' => 'btn btn-primary')) }}</td>
             </tr>
